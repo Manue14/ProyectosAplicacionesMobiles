a1881bf77a173af008be3a74d287fb4e49b64c70
Manuel Álvarez Gómez <a21manuelag@teis.intranet>
2024-10-21 10:41:19 +0200

    Componentes y eventos

--- EjerciciosInterfaces/app/src/main/java/com/example/ejerciciosinterfaces/MainActivity.java
+++ EjerciciosInterfaces/app/src/main/java/com/example/ejerciciosinterfaces/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.ejerciciosinterfaces;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewRespuesta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pregunta);
+        textViewRespuesta = findViewById(R.id.view_respuesta);
+    }
+
+    public void onClickBtnSi(View view) {
+        textViewRespuesta.setText("Has pulsado el botón Sí");
+    }
+
+    public void onClickBtnNo(View view) {
     }
 }
