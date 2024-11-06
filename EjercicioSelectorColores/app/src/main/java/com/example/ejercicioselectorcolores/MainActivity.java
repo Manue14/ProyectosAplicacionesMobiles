b0abbdff49725e6b43c5f7d97b5a157a71f0f2c2
Manue14 <mnalvarezgomez@gmail.com>
2024-11-07 00:39:57 +0100

    Casi acabado el ejercicio del selector de colores

--- EjercicioSelectorColores/app/src/main/java/com/example/ejercicioselectorcolores/MainActivity.java
+++ EjercicioSelectorColores/app/src/main/java/com/example/ejercicioselectorcolores/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.ejercicioselectorcolores;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +12,153 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.HashMap;
 
+public class MainActivity extends AppCompatActivity {
+    private SeekBar skbRed, skbGreen, skbBlue, skbAlpha;
+    private TextView txtRed, txtGreen, txtBlue, txtAlpha;
+    private View viewColor;
+    private int redValue, greenValue, blueValue, alphaValue;
+    private HashMap<Integer, String> equivalencies = new HashMap<Integer, String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+        updateColorView();
+
+        this.skbRed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtRed.setText(String.valueOf(i));
+                redValue = i;
+                updateColorView();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        this.skbGreen.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtGreen.setText(String.valueOf(i));
+                greenValue = i;
+                updateColorView();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        this.skbBlue.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtBlue.setText(String.valueOf(i));
+                blueValue = i;
+                updateColorView();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        this.skbAlpha.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtAlpha.setText(String.valueOf(i));
+                alphaValue = i;
+                updateColorView();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void initialize() {
+        this.skbRed = findViewById(R.id.red_bar);
+        this.skbGreen = findViewById(R.id.green_bar);
+        this.skbBlue = findViewById(R.id.blue_bar);
+        this.skbAlpha = findViewById(R.id.alpha_bar);
+        this.txtRed = findViewById(R.id.txt_red_value);
+        this.txtGreen = findViewById(R.id.txt_green_value);
+        this.txtBlue = findViewById(R.id.txt_blue_value);
+        this.txtAlpha = findViewById(R.id.txt_alpha_value);
+        this.viewColor = findViewById(R.id.view_color);
+
+        this.redValue = this.skbRed.getProgress();
+        this.greenValue = this.skbGreen.getProgress();
+        this.blueValue = this.skbBlue.getProgress();
+        this.alphaValue = this.skbAlpha.getProgress();
+
+        this.txtRed.setText(String.valueOf(this.redValue));
+        this.txtGreen.setText(String.valueOf(this.greenValue));
+        this.txtBlue.setText(String.valueOf(this.blueValue));
+        this.txtAlpha.setText(String.valueOf(this.alphaValue));
+
+        this.equivalencies.put(0, "0");
+        this.equivalencies.put(1, "1");
+        this.equivalencies.put(2, "2");
+        this.equivalencies.put(3, "3");
+        this.equivalencies.put(4, "4");
+        this.equivalencies.put(5, "5");
+        this.equivalencies.put(6, "6");
+        this.equivalencies.put(7, "7");
+        this.equivalencies.put(8, "8");
+        this.equivalencies.put(9, "9");
+        this.equivalencies.put(10, "A");
+        this.equivalencies.put(11, "B");
+        this.equivalencies.put(12, "C");
+        this.equivalencies.put(13, "D");
+        this.equivalencies.put(14, "E");
+        this.equivalencies.put(15, "F");
+    }
+
+    private void updateColorView() {
+        String hexValue = "#" + intToHex(this.alphaValue) + intToHex(this.redValue) +
+                intToHex(this.greenValue) + intToHex(this.blueValue);
+        System.out.println(hexValue);
+        this.viewColor.setBackgroundColor(Color.parseColor(hexValue));
+    }
+
+    private String intToHex(int i) {
+        String hex = "";
+        do {
+            hex = this.equivalencies.get(i % 16) + hex;
+            i = i / 16;
+        } while (i > 0);
+        if (hex.length() < 2) {
+            hex = "0" + hex;
+        }
+        return hex;
     }
 }
